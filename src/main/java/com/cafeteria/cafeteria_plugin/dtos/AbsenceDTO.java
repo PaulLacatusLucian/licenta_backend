a2adb0e3c67bbd0f8f5c86fcdf30674dc310c942
PaulLacatusLucian <paul2003mai@yahoo.com>
2025-05-06 19:56:58 +0300

    Improvements

--- src/main/java/com/cafeteria/cafeteria_plugin/dtos/AbsenceDTO.java
+++ src/main/java/com/cafeteria/cafeteria_plugin/dtos/AbsenceDTO.java
@@ -2,10 +2,13 @@ package com.cafeteria.cafeteria_plugin.dtos;
 
 import lombok.Data;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 public class AbsenceDTO {
     private Long id;
     private StudentDTO student;
     private Long classSessionId;
+    private LocalDateTime sessionDate;
+    private TeacherDTO teacherWhoMarkedAbsence;
 }
